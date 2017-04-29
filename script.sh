900ea6074edb7e442e9880174fc8f3597b151a4d
srikk595 <srikk595@gmail.com>
2017-04-29 12:27:59 -0700

    Create script to generate symlinks

--- script.sh
+++ script.sh
@@ -6,8 +6,23 @@
 # Distributed under terms of the MIT license.
 #
 
-
-ln -s ~/.tmux.conf ~/dotfiles/.tmux.conf
-ln -s ~/.tmux.conf ~/dotfiles/.vim
-ln -s ~/.tmux.conf ~/dotfiles/.vimrc
-ln -s ~/tmux-powerline ~/dotfiles/tmux-powerline
+cd ~
+echo In directory: $PWD
+echo Cleaning dotfile related symlinks
+rm ~/.oh-my-zsh
+rm ~/.tmux.conf
+rm ~/.vim
+rm ~/.vimrc
+rm ~/.zsh
+rm ~/.zshrc
+echo Creating symlinks
+ln -s ~/workspace/dotfiles1/oh-my-zsh .oh-my-zsh
+ln -s ~/workspace/dotfiles1/.tmux.conf .tmux.conf
+ln -s ~/workspace/dotfiles1/.vim .vim
+ln -s ~/workspace/dotfiles1/.vimrc .vimrc
+ln -s ~/workspace/dotfiles1/vincentbernat/zshrc .zsh
+ln -s ~/workspace/dotfiles1/.zshrc .zshrc
+cd ~/workspace/dotfiles1/oh-my-zsh/themes/
+echo In directory: $PWD
+echo Creating symlinks for oh-my-zsh themes
+ln -s ~/workspace/dotfiles1/honukai-iterm-zsh/honukai.zsh-theme honukai.zsh-theme
